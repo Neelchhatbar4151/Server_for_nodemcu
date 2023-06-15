f45740f91ad2262760e5cc9349249fc590505c47
neelchhatbar <neelchhatbar@gmail.com>
2023-06-15 17:13:50 +0530

    project almost complete

---
+++ UserScheme.js
@@ -0,0 +1,25 @@
+const Mongoose = require('mongoose');
+
+const UserScheme = new Mongoose.Schema({
+      Celsius: {
+            type: Number,
+      },
+      Fahrenheit: {
+            type: Number,
+      }
+})
+
+const User = Mongoose.model("Temperatures", UserScheme);
+
+const DB = "mongodb+srv://Fake:demopassword123@cluster0.owymoti.mongodb.net/?retryWrites=true&w=majority";
+
+Mongoose.connect(DB,{
+      useNewUrlParser : true,
+      useUnifiedTopology:true,
+}).then(()=>{
+      console.log("connection successfull");
+}).catch((err)=>{
+      console.log(err);
+})
+
+module.exports = User;
--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 const express = require("express");
 const R = express.Router();
+const Mongoose = require("mongoose");
 
-let t = 10;
-R.get("/",(req,res)=>{
+const User = require("./UserScheme");
+
+let t = 0;
+R.get("/", (req, res) => {
       console.log(t)
       return res.status(200).json({ status: 200 });
 })
 
-R.get("/read/:te", (req,res)=>{
-      console.log("Hii")
-      const {te} = req.params;
-      console.log(req.params)
-      t = te;
-      console.log("Post: "+t)
-      return res.status(200).json({status: 200});
+const add = async (ce, fa) => {
+      try {
+            const user = new User({ Celsius: ce, Fahrenheit: fa });
+            await user.save();
+      } catch (error) {
+            console.log(error);
+      }
+}
+
+R.get("/read/:ce", async (req, res) => {
+      const { ce } = req.params;
+      t = ce;
+      console.log("Celsius: " + t)
+      const F = (t * 9) / 5 + 32
+      console.log("Fahrenheit: " + F)
+      await add(t, F);
+      return res.status(200).json({ status: 200 });
 })
 
 
